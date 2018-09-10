dce1fd35fe4ff58bdf4bb2f12d9817f4a7973023
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-09-09 23:08:38 -0300

    fix 1/99 problems in clojure

--- src/p99/core.clj
+++ src/p99/core.clj
@@ -16,8 +16,8 @@
   ;; This is a clojure form. Enter a value which will make the form evaluate to
   ;; true. Don't over think it! If you are confused, see the getting started
   ;; page. Hint: true is equal to true.
-  nil)
-
+  (true? (= 1 1))
+)
 
 ;; problem 2 (Elementary)
 (def simple-math-solution
